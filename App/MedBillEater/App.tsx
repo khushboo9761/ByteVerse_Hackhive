0b39bdceafed1dd45449950a65e5527113446ab1
Trisha Halder <98026599+TRISHAHALDER@users.noreply.github.com>
2024-04-14 19:14:04 +0530

    Update App.tsx

--- App/MedBillEater/App.tsx
+++ App/MedBillEater/App.tsx
@@ -1,54 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { View, Text, Button } from 'react-native';
-// import axios from 'axios';
-
-// interface Task {
-//   id: number;
-//   title: string;
-//   completed: boolean;
-// }
-
-// const App: React.FC = () => {
-//   const [tasks, setTasks] = useState<Task[]>([]);
-
-//   useEffect(() => {
-//     fetchTasks();
-//   }, []);
-
-//   const fetchTasks = async () => {
-//     try {
-//       const response = await axios.get<Task[]>('http://192.168.87.218:3000/tasks');
-//       setTasks(response.data);
-//     } catch (error) {
-//       console.error('Error fetching tasks:', error);
-//     }
-//   };
-
-//   const createTask = async () => {
-//     try {
-//       await axios.post('http://192.168.87.218:3000/tasks', {
-//         id: tasks.length + 1,
-//         title: `Task ${tasks.length + 1}`,
-//         completed: false
-//       });
-//       fetchTasks();
-//     } catch (error) {
-//       console.error('Error creating task:', error);
-//     }
-//   };
-
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Tasks:</Text>
-//       {tasks.map(task => (
-//         <Text key={task.id}>{task.title}</Text>
-//       ))}
-//       <Button title="Create Task" onPress={createTask} />
-//     </View>
-//   );
-// };
-
-//  export default App; // Correct export statement
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React from 'react';
 import Signin from './src/Signin';
@@ -58,6 +7,7 @@ import FilesUpload from './src/components/Fileupload';
 import ImagePicker from './src/components/Imagepicker';
 import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import MedicalBillSummary from './src/components/BillSummary';
 const Stack = createStackNavigator();
 const MyDarkTheme = {
  // Extend the DefaultTheme
@@ -79,6 +29,7 @@ const App : React.FC = () => {
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="FilesUpload" component={FilesUpload} />
+      
     </Stack.Navigator>
   </NavigationContainer> 
 
